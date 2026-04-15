e170f6bef634b3ab99f95d1728bfba30461d8b8f
Genci <cng4517@gmail.com>
2026-04-15 02:00:51 +0200

    Codex stuff

--- components/landing/landing-page.jsx
+++ components/landing/landing-page.jsx
@@ -11,8 +11,8 @@ export function LandingPage() {
     <main className="relative isolate min-h-screen overflow-hidden bg-transparent text-white antialiased">
       <AnimatedBackground />
 
-      <div className="relative z-10 px-2 py-2 sm:px-3 sm:py-3 lg:px-4 lg:py-4">
-        <div className="mx-auto min-h-[calc(100vh-1rem)] max-w-[1600px] overflow-hidden rounded-[2rem] border border-white/8 bg-[#0b0b0c] shadow-[0_40px_140px_rgba(0,0,0,0.38)]">
+      <div className="relative z-10">
+        <div className="mx-auto min-h-screen max-w-[1600px] overflow-hidden bg-[#0b0b0c] shadow-[0_40px_140px_rgba(0,0,0,0.3)]">
           <Navbar />
           <HeroSection />
           <ReasonsSection />
